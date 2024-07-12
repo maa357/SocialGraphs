22b0bffa24ecc281080c82372380fc12884e196c
Mohamad Hani Alwaw <alwawhany3@gmail.com>
2024-07-12 15:06:42 +0300

    clearFileContent function

--- Interface.py
+++ Interface.py
@@ -69,5 +69,8 @@ def writeGraphOnFile(network, file_path):
         for row in network:
             row_str = ','.join(map(str, row))
             file.write(row_str + '\n')
+def clearFileContent(file_path):
+    with open(file_path, 'w') as file:
+        pass
 
 main()
